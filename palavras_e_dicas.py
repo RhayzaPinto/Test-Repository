b5ad2bc8e5b46a3462f702d0cdc8074662d5b66d
Rhayza Pinto <rhayzapinto@gmail.com>
2021-12-14 17:07:39 -0300

    Implementando dicas

---
+++ palavras_e_dicas.py
@@ -0,0 +1,16 @@
+palavras = ["Sintaxe", "Sequência", "Estritamente", "concatenação", "Unária", "Inteligência", "Respeito",
+              "Teclado", "Software", "Aprendigem", "Indentação", "Algoritmo", "Programação", "Autoconhecimento",
+              "Condicional", "Webinar", "Hardware", "Desenvolvimento", "Atribuição", "Escopo"]
+
+dicas = ["Componente do sistema linguístico", "Sucessão", "Rigorosamente", "Relacionamento de ideias", "Única", "Faculdade de conhecer",
+         "Consideração, deferência", "Instrumento para digitar", "Suporte lógico", "Experiência ou prática", "Espaço entre a margem e o começo da linha escrita",
+         "Sequência finita de ações executáveis", "Forma como nos comunicamos com o computador", "Pilar da inteligência emocional",
+         "Sujeito a, dependente de", "Videoconferência", "Parte física do computador", "Crescimento, progresso", "Cargo, trabalho ou função",
+         "Atividade ou pensamento desimpedido"]
+
+def palavraAleatoria(index):
+    return palavras[index]
+
+
+def dicaAleatoria(index):
+    return dicas[index]
